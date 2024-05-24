de8e093d1bf7f57059cc8ff65fea549e9e665b1f
Simran Kamboj <72260678+SimranKKamboj@users.noreply.github.com>
2024-05-24 00:46:50 -0700

    Update simran.js

--- assets/js/simran.js
+++ assets/js/simran.js
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* JohnDoe Landing page
+* Simran Landing page
 =========================================================
 
 * Copyright: 2019 DevCRUD (https://devcrud.com)
